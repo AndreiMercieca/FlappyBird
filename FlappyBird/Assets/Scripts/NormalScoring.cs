087d586b29844cb4dbfbeb8dd58b03fc471e990f
AndreiMercieca <74543545+AndreiMercieca@users.noreply.github.com>
2021-01-19 15:19:22 +0100

    Finalized Flappy Bird
    Added score system
    Added bubbles for extra points
    and added the original scoring system

---
+++ FlappyBird/Assets/Scripts/NormalScoring.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalScoring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            //create a reference to the scorekeeper script
+            ScoreKeeper scoreKeeper = GameObject.FindObjectOfType<ScoreKeeper>();
+            //we need to check if the script was found
+            if(scoreKeeper != null)
+            {
+                //Increment Score
+                scoreKeeper.IncrementScore();
+            }
+        }
+    }
+}
